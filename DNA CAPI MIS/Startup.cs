a2f73b4fbdf757feb6968b1a9ab06a3a0e44327a
saqib.taj <staj@macrosoftinc.com>
2025-09-08 21:01:58 +0500

    add core library

--- DNA CAPI MIS/Startup.cs
+++ DNA CAPI MIS/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Web.Http;
 
 [assembly: OwinStartupAttribute(typeof(DNA_CAPI_MIS.Startup))]
 namespace DNA_CAPI_MIS
@@ -8,6 +9,12 @@ namespace DNA_CAPI_MIS
     {
         public void Configuration(IAppBuilder app)
         {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Enable Web API
+            WebApiConfig.Register(config);
+
+            // Allow CORS via OWIN
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
---
+++ DNA CAPI MIS/Utility/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+public static class WebApiConfig
+{
+    public static void Register(HttpConfiguration config)
+    {
+        // Enable CORS globally
+        var cors = new EnableCorsAttribute("*", "*", "*");
+        config.EnableCors(cors);
+
+        config.MapHttpAttributeRoutes();
+
+        config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+        );
+    }
+}
