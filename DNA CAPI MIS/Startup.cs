483d7e020767e27a4e54ab6464607def99400272
saqib.taj <staj@innovatixinc.com>
2025-10-14 23:11:23 +0500

    Update

--- DNA CAPI MIS/Startup.cs
+++ DNA CAPI MIS/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 [assembly: OwinStartupAttribute(typeof(DNA_CAPI_MIS.Startup))]
 namespace DNA_CAPI_MIS
@@ -11,11 +12,13 @@ namespace DNA_CAPI_MIS
         {
             HttpConfiguration config = new HttpConfiguration();
 
+            // Enable CORS globally
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
             // Enable Web API
             WebApiConfig.Register(config);
 
-            // Allow CORS via OWIN
-            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }
--- DNA CAPI MIS/Utility/WebApiConfig.cs
+++ DNA CAPI MIS/Utility/WebApiConfig.cs
@@ -1,14 +1,9 @@
 ﻿using System.Web.Http;
-using System.Web.Http.Cors;
 
 public static class WebApiConfig
 {
     public static void Register(HttpConfiguration config)
     {
-        // Enable CORS globally
-        var cors = new EnableCorsAttribute("*", "*", "*");
-        config.EnableCors(cors);
-
         config.MapHttpAttributeRoutes();
 
         config.Routes.MapHttpRoute(
